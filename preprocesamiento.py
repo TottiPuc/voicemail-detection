045cb41e8843bbd9c173dd76541cd66f1bbf2833
Christian Arcos <dayan3846@gmail.com>
2022-03-07 22:11:58 -0500

    flake8

--- preprocesamiento.py
+++ preprocesamiento.py
@@ -10,30 +10,33 @@ from sklearn import preprocessing
 axes = []
 
 
+def plot_specgram(file_path):
+
+    for m, path in enumerate(file_path):
+        fig = plt.figure(figsize=(10, 5))
+        for n, file in enumerate(os.listdir(path)):
+            y, sr = librosa.load(path + file)
+            S = librosa.feature.melspectrogram(y=y,\
+                                               sr=sr, n_mels=128, fmax=8000)
+            axes.append(fig.add_subplot(4, 5, n + 1))
+            librosa.display.specshow(librosa.power_to_db(S, \
+                                                         ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
+            plt.colorbar(format='%+2.0f dB')
+            plt.title(file_path[m].split("/")[-2])
+        plt.tight_layout()
+        plt.show()
+
+
 class Preprocesamiento():
 
     def __init__(self, path_data):
         self.path_data = path_data
-        self.df = pd.DataFrame(columns=['file_path', 'mfccs_40', 'chroma', 'mel', 'contrast',\
-                                        'tonnetz', 'duration', 'label'])
+        self.df = pd.DataFrame(
+            columns=['file_path', 'mfccs_40', 'chroma', 'mel', \
+                     'contrast', 'tonnetz', 'duration', 'label'])
         self.df = self.df.fillna(0)  # with 0s rather than NaNs
         print('iniciando preprocesamiento de audios con caracteristicas almacenadas en {}'\
               .format(path_data))
-
-    def plot_specgram(self, file_path):
-
-        for m, path in enumerate(file_path):
-            fig = plt.figure(figsize=(10, 5))
-            for n, file in enumerate(os.listdir(path)):
-                y, sr = librosa.load(path + file)
-                S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-                axes.append(fig.add_subplot(4, 5, n + 1))
-                librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel',\
-                                         fmax=8000, x_axis='time')
-                plt.colorbar(format='%+2.0f dB')
-                plt.title(file_path[m].split("/")[-2])
-            plt.tight_layout()
-            plt.show()
 
     def process(self, file, label):
         try:
@@ -43,20 +46,15 @@ class Preprocesamiento():
             X, sample_rate = librosa.load(file, res_type='kaiser_fast')
             duration = librosa.get_duration(y=X, sr=sample_rate)
             stft = np.abs(librosa.stft(X))
-            mfccs_40 = np.mean(librosa.feature.mfcc(y=X, \
-                                                    sr=sample_rate, n_mfcc=40).T, axis=0)
-            chroma = np.mean(librosa.feature.chroma_stft(S=stft, \
-                                                         sr=sample_rate).T, axis=0)
-            mel = np.mean(librosa.feature.melspectrogram(y=X, \
-                                                         sr=sample_rate, n_mels=128, fmax=8000).T, axis=0)
-            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, \
-                                                                 sr=sample_rate).T, axis=0)
-            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),\
+            mfccs_40 = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+            chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+            mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate, n_mels=128, fmax=8000).T, axis=0)
+            contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T, axis=0)
+            tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
                                                       sr=sample_rate).T, axis=0)
 
             row = pd.Series(
-                {'file_path': file, 'mfccs_40': mfccs_40, 'chroma': chroma,\
-                 'mel': mel, 'contrast': contrast, \
+                {'file_path': file, 'mfccs_40': mfccs_40, 'chroma': chroma, 'mel': mel, 'contrast': contrast,
                  'tonnetz': tonnetz, 'duration': duration, 'label': label}, name=7)
             self.df.loc[len(self.df)] = row
 
@@ -67,7 +65,7 @@ class Preprocesamiento():
         index = 0
         for file in glob.glob(path):
             if index <= max:
-                # row = self.process(file, label)
+                self.process(file, label)
                 index = index + 1
         self.df.dropna(inplace=True)
         self.df.to_pickle(self.path_data + 'voicemail_dataset_train.pkl')
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from preprocesamiento import Preprocesamiento
+from preprocesamiento import Preprocesamiento, plot_specgram
 from entrenamiento import Entrenamiento
 from modelos import modelos
 
@@ -8,9 +8,10 @@ path_gente=RECORDINGS_PATH + "gente/"
 path_dataset= '/home/dayan/Documents/VOZY/voicemial/dataset/'
 path_models= '/home/dayan/Documents/VOZY/voicemial/modelos/'
 classi = 'lr'
+plot_specgram([path_maquina,path_gente])
 
 inicio = Preprocesamiento(path_dataset)
-#inicio.plot_specgram([path_maquina,path_gente])
+
 inicio.crear_dataset(path_maquina+"*.wav", label="beep")
 inicio.crear_dataset(path_gente+"*.wav", label="speech")
 print(inicio.df.label.value_counts())
@@ -21,7 +22,8 @@ class_names=inicio.one_hot_encoding()
 # rf= randomForest
 # xgb = xgboost
 # svm = suport vetor machine
-
+'''
 model = modelos(model=classi, solver='liblinear', max_iter=4000)
 train = Entrenamiento(inicio.df, model)
 train.train(['mfccs_40'],class_names,classi,path_models)
+'''
